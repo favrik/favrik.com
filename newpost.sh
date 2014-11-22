ae3d2c7a304a800fbc9e9b288f84a0895297c9ad
Favio Manriquez Leon <favio@favrik.com>
2014-11-22 00:01:50 -0800

    Remove unused script

--- newpost.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DRAFTDIR=./blog/_drafts/
-FILE=$DRAFTDIR$(date +%F)-$(echo $1 | tr [A-Z] [a-z] | sed 's/ \|\./-/g').markdown
-#echo $1
-#echo $FILE
-touch $FILE
-echo "---\nlayout: post\ntitle: $1\nmeta_description: \nmeta_keywords: \n---\n" > $FILE
-gvim $FILE
